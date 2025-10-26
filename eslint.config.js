c5498923228e9beeb29543048532914d27ae2c23
Victor Farias <gfvictor@proton.me>
2025-10-26 13:50:06 +0900

    chore: update eslint.config and delete .eslintrc

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import typescriptEslintParser from '@typescript-eslint/parser';
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    ignores: ['node_modules/**', 'dist/**'],
+    languageOptions: {
+      parser: typescriptEslintParser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...typescriptEslintPlugin.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
